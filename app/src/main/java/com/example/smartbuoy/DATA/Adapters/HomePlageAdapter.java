28314874d3a29d149741bcadf38f74effcb6515d
firas kordoghli <firas9@rocketmail.com>
2019-08-22 20:04:45 +0100

    searsh bar done

--- app/src/main/java/com/example/smartbuoy/DATA/Adapters/HomePlageAdapter.java
+++ app/src/main/java/com/example/smartbuoy/DATA/Adapters/HomePlageAdapter.java
@@ -3,6 +3,8 @@ package com.example.smartbuoy.DATA.Adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,16 +15,44 @@ import com.example.smartbuoy.DATA.Models.ItemHomePlage;
 import com.example.smartbuoy.R;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class HomePlageAdapter extends RecyclerView.Adapter<HomePlageAdapter.HomePlageViewHolder> {
+public class HomePlageAdapter extends RecyclerView.Adapter<HomePlageAdapter.HomePlageViewHolder> implements Filterable {
     private List<ItemHomePlage> mHomePlagesList;
     private List<ItemHomePlage> mHomePlagesListFull;
     private OnItemClickListener mListener;
+    private Filter plageFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            List<ItemHomePlage> filteredList = new ArrayList<>();
+
+            if (charSequence == null || charSequence.length() == 0) {
+                filteredList.addAll(mHomePlagesListFull);
+            } else {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+                for (ItemHomePlage item : mHomePlagesListFull) {
+                    if (item.getName().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            mHomePlagesList.clear();
+            mHomePlagesList.addAll((List) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public HomePlageAdapter(List<ItemHomePlage> itemHomePlages) {
         mHomePlagesList = itemHomePlages;
-
+        mHomePlagesListFull = new ArrayList<>(itemHomePlages);
     }
 
     public void setOnItemClickListener(OnItemClickListener listener) {
@@ -52,6 +82,11 @@ public class HomePlageAdapter extends RecyclerView.Adapter<HomePlageAdapter.Home
     @Override
     public int getItemCount() {
         return mHomePlagesList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return plageFilter;
     }
 
     public interface OnItemClickListener {
--- app/src/main/java/com/example/smartbuoy/UI/SearshActivity.java
+++ app/src/main/java/com/example/smartbuoy/UI/SearshActivity.java
@@ -6,7 +6,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.example.smartbuoy.DATA.Adapters.HomePlageAdapter;
 import com.example.smartbuoy.DATA.Models.ItemHomePlage;
@@ -24,10 +29,15 @@ public class SearshActivity extends AppCompatActivity {
     private HomePlageAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
 
+    private androidx.appcompat.widget.Toolbar searshTopToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searsh);
+
+        searshTopToolbar = (androidx.appcompat.widget.Toolbar) findViewById(R.id.searsh_toolbar);
+        setSupportActionBar(searshTopToolbar);
 
         mRecyclerView = findViewById(R.id.rvSearsh);
         mRecyclerView.setHasFixedSize(true);
@@ -66,5 +76,44 @@ public class SearshActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.searsh_menu,menu);
+
+        MenuItem searchItem = menu.findItem(R.id.actionSearch);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                mAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.actionSearch) {
+            Toast.makeText(SearshActivity.this, "Action clicked", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
